66294732bd33d00015f4370e807fb600798f1685
Yair Mor <yairmor28@gmail.com>
2017-11-13 21:27:09 +0200

    טלאופ

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleopTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleopTest.java
@@ -15,12 +15,16 @@ public class teleopTest extends OpMode {
     DcMotor right;
     Servo ser;
     Servo ser2;
+    DcMotor glifs1;
+    DcMotor glifs2;
     @Override
     public void init() {
         left = hardwareMap.dcMotor.get("MotorLeft");
         right = hardwareMap.dcMotor.get("MotorRight");
-        //ser = hardwareMap.servo.get("Servo");
-        //ser2 = hardwareMap.servo.get("Servo2");
+        ser = hardwareMap.servo.get("Servo");
+        ser2 = hardwareMap.servo.get("Servo2");
+        glifs1 = hardwareMap.dcMotor.get("glifs1");
+        glifs2 = hardwareMap.dcMotor.get("glifs2");
         right.setDirection(DcMotorSimple.Direction.REVERSE);
     }
 
@@ -29,13 +33,20 @@ public class teleopTest extends OpMode {
         left.setPower(gamepad1.left_stick_y);
         right.setPower(gamepad1.right_stick_y);
         if(gamepad2.a){
-           //ser.setPosition(-0.5);
-           // ser2.setPosition(0.5);
+           glifs1.setPower(1);
+           glifs2.setPower(1);
         }
         else{
-            //ser.setPosition(1);
-            //ser2.setPosition(1);
+           glifs1.setPower(0);
+           glifs2.setPower(0);
         }
+        if(gamepad2.y){
+            ser.setPosition(0.8);
+            ser2.setPosition(-0.8);
+
+        } else
+            ser.setPosition(0.2);
+                ser2.setPosition(-0.2);
     }
 }
 
